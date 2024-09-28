970e06694fc91b2b860365075412c30475e2ef46
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-09-28 18:44:08 +0530

    :lipstick: added category and ellipsis using :package: react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,44 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HiEllipsisVertical, HiMiniBars3BottomRight } from "react-icons/hi2";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
   const [showCategories, setShowCategories] = useState(false);
-  return <nav></nav>;
+  return (
+    <nav>
+      <div className="relative flex justify-between items-center mb-2">
+        <Link to="/" className="flex gap-2">
+          <img src="/logo.svg" alt="logo" className="w-8" />
+          <h1 className="text-5xl font-bold tracking-tighter cursor-pointer">
+            GIPHY
+          </h1>
+        </Link>
+
+        {/* render categories */}
+        <Link className="px-4 py-1 hover:gradient border-b-4 hidden lg:block">
+          Reactions
+        </Link>
+        <button onClick={() => setShowCategories(!showCategories)}>
+          <HiEllipsisVertical
+            size={35}
+            className={`${
+              showCategories ? "gradient" : ""
+            } py-0.5 hover:gradient border-b-4 hidden lg:block`}
+          />
+        </button>
+        <button className="bg-gray-700 py-1.5 px-6 rounded cursor-pointer">
+          <Link to="/favorites">Favorite GIFs</Link>
+        </button>
+        <button>
+          <HiMiniBars3BottomRight
+            size={30}
+            className="text-sky-400 block lg:hidden"
+          />
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
