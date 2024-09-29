c79425fa19473da888c137d4709c9ea3c3f681be
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-09-29 16:50:31 +0530

    :zap: :lipstick: added filters

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { GIFState } from "../context/gifContext";
 import Gif from "../components/Gif";
+import FilterGif from "../components/FilterGif";
 
 const Home = () => {
   const { gf, gifs, filter, setGifs } = GIFState();
@@ -25,6 +26,10 @@ const Home = () => {
         alt="earth banner"
         className="my-2 rounded w-full"
       />
+
+      {/* Filter GIFs */}
+      <FilterGif showTrending />
+
       <div className="columns-2 sm:columns-3 md:columns-4 lg:columns-5 xl:columns-6 gap-2">
         {gifs?.map((gif) => (
           <Gif gif={gif} key={gif.title} />
