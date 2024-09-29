5f8191dd8be809d40c9ba200b46567edf5368738
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-09-29 13:49:51 +0530

    :zap: :art: :electric_plug: fetch trending gif on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+import { GIFState } from "../context/gifContext";
+import Gif from "../components/Gif";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { gf, gifs, filter, setGifs } = GIFState();
 
-export default Home
+  const fetchTrendingGIFs = async () => {
+    const { data } = await gf.trending({
+      limit: 20,
+      type: filter,
+      rating: "g",
+    });
+    setGifs(data);
+  };
+
+  useEffect(() => {
+    fetchTrendingGIFs();
+  }, []);
+
+  return (
+    <div>
+      <img
+        src="/banner.gif"
+        alt="earth banner"
+        className="mt-2 rounded w-full"
+      />
+      {gifs?.map((gif) => (
+        <Gif gif={gif} key={gif.title} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
