4f5ce69182d4d617dc57fcc87ce58e878343a90f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-09-29 12:51:01 +0530

    :zap: :bulb: GIF context with :key: API KEY

--- src/context/gifContext.jsx
+++ src/context/gifContext.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable react/prop-types */
-import { createContext } from "react";
+import { GiphyFetch } from "@giphy/js-fetch-api";
+import { createContext, useContext } from "react";
 
 const GIFContext = createContext();
 
 const GIFProvider = ({ children }) => {
-  return <GIFContext.Provider>{children}</GIFContext.Provider>;
+  const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_API_KEY);
+
+  return <GIFContext.Provider value={{ gf }}>{children}</GIFContext.Provider>;
+};
+
+export const GIFState = () => {
+  return useContext(GIFContext);
 };
 
 export default GIFProvider;
