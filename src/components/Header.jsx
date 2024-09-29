ef48b533237b5eeffde9135235b7e3b65d906e5f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-09-29 13:30:30 +0530

    :art: :loop: render categories with gradient

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { HiEllipsisVertical, HiMiniBars3BottomRight } from "react-icons/hi2";
+import { GIFState } from "../context/gifContext";
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
   const [showCategories, setShowCategories] = useState(false);
+
+  const { gf, filter, setFilter, favorites } = GIFState();
+
+  const fetchCategories = async () => {
+    const { data } = await gf.categories();
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
   return (
     <nav>
       <div className="relative flex justify-between items-center mb-2">
@@ -16,9 +28,17 @@ const Header = () => {
         </Link>
         <div className="font-bold text-md flex gap-2 items-center">
           {/* render categories */}
-          <Link className="px-4 py-1 hover:gradient border-b-4 hidden lg:block">
-            Reactions
-          </Link>
+          {categories?.slice(0, 5).map((category, idx) => {
+            return (
+              <Link
+                className="px-4 py-1 hover:gradient border-b-4 hidden lg:block"
+                key={idx}
+              >
+                {category.name}
+              </Link>
+            );
+          })}
+
           <button onClick={() => setShowCategories(!showCategories)}>
             <HiEllipsisVertical
               size={35}
@@ -27,9 +47,11 @@ const Header = () => {
               } py-0.5 hover:gradient border-b-4 hidden lg:block`}
             />
           </button>
-          <button className="bg-gray-700 py-1.5 px-6 rounded cursor-pointer">
-            <Link to="/favorites">Favorite GIFs</Link>
-          </button>
+          {favorites.length > 1 && (
+            <button className="bg-gray-700 py-1.5 px-6 rounded cursor-pointer">
+              <Link to="/favorites">Favorite GIFs</Link>
+            </button>
+          )}
           <button>
             <HiMiniBars3BottomRight
               size={30}
@@ -38,11 +60,21 @@ const Header = () => {
           </button>
         </div>
         {showCategories && (
-          <div className="absolute right-0 top-14 px-10 pt-6 w-full z-20 gradient">
-            <span>Catgories</span>
-            <hr />
-            <div>
-              <Link className="font-bold">Reactions</Link>
+          <div className="absolute right-0 top-14 px-10 pt-6 pb-9 w-full z-20 gradient">
+            <span className="font-extrabold text-3xl">Catgories</span>
+            <hr className="bg-gray-200 opacity-50 my-5" />
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+              {categories?.map((category, idx) => {
+                return (
+                  <Link
+                    className="font-bold"
+                    key={idx}
+                    to={`/${category.name_encoded}`}
+                  >
+                    {category.name}
+                  </Link>
+                );
+              })}
             </div>
           </div>
         )}
--- src/context/gifContext.jsx
+++ src/context/gifContext.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable react/prop-types */
 import { GiphyFetch } from "@giphy/js-fetch-api";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const GIFContext = createContext();
 
 const GIFProvider = ({ children }) => {
+  const [gifs, setGifs] = useState([]);
+  const [filter, setFilter] = useState("gifs");
+  const [favorites, setFavorites] = useState([]);
+
   const gf = new GiphyFetch(import.meta.env.VITE_GIPHY_API_KEY);
 
-  return <GIFContext.Provider value={{ gf }}>{children}</GIFContext.Provider>;
+  return (
+    <GIFContext.Provider
+      value={{ gf, gifs, setGifs, filter, setFilter, favorites }}
+    >
+      {children}
+    </GIFContext.Provider>
+  );
 };
 
 export const GIFState = () => {
